1a1a92102ecad0ed2e4a31e27557d02853e455ef
ImUrX <urielfontan2002@gmail.com>
2023-02-03 16:00:13 -0300

    vscode addons

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -18,14 +18,18 @@ in
           package = pkgs.vscodium;
           extensions = with pkgs.vscode-extensions;
             [
-              matklad.rust-analyzer
-              tamasfe.even-better-toml
               jnoortheen.nix-ide
               kamadorueda.alejandra
-              dbaeumer.vscode-eslint
-              redhat.vscode-xml
-              esbenp.prettier-vscode
               eamodio.gitlens
+              redhat.vscode-xml
+              editorconfig.editorconfig
+              # Javascript
+              dbaeumer.vscode-eslint
+              esbenp.prettier-vscode
+              bradlc.vscode-tailwindcss
+              # Rust
+              matklad.rust-analyzer
+              tamasfe.even-better-toml
             ]
             ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
               {
@@ -33,6 +37,12 @@ in
                 publisher = "macabeus";
                 version = "1.0.0";
                 sha256 = "814e374452e9e9e99896893ad1c1de2dae2486285a668f985c66ef25eb08a9f2";
+              }
+              {
+                name = "flatbuffers";
+                publisher = "gaborv";
+                version = "0.1.0";
+                sha256 = "dd6b26d628ade5e0b99e32c52db1a184060d6299c0589afbe0e96c46042a0acb";
               }
             ];
         };
